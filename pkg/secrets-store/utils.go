e0f7194c140327c8fc65e75ce5ab02fafec77185
bryan leighton <bryan.leigh77@gmail.com>
2020-06-13 07:51:35 -0700

    --amend

--- pkg/secrets-store/utils.go
+++ pkg/secrets-store/utils.go
@@ -17,12 +17,16 @@ limitations under the License.
 package secretsstore
 
 import (
+	corev1 "k8s.io/api/core/v1"
+
 	"fmt"
 	"io/ioutil"
 	"os"
 	"regexp"
 	"runtime"
 	"strings"
+
+	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
